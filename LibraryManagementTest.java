076b8ad1f5fc5d12f773abb4837b76b49fdd448e
Kyle Z. Yang <kyleyang@trentu.ca>
2024-12-01 03:05:33 -0500

    bookID test

--- LibraryManagementTest.java
+++ LibraryManagementTest.java
@@ -1,12 +1,33 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
 
-class LibraryManagementTest {
+public class LibraryManagementTest {
 
+	private Book book;
+	
+	@Before
+	public void setUp() {
+		book = new Book(0, "Placeholder");// values do literally nothing, its just so I can use the isvalid function
+	}
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testBookValid() {
+		//valid tests
+		assertTrue("100 is a valid id", book.isValidId(50));
+		assertTrue("295 is a valid id", book.isValidId(295));
+		assertTrue("999 is a valid id", book.isValidId(999));
+		
+		
+		//invalid tests
+		assertFalse("99 is not a valid id", book.isValidId(99));
+		assertFalse("-4 is not a valid id", book.isValidId(-4));
+		assertFalse("1000 is not a valid id", book.isValidId(1000));
+		
+		
+		
+		
+		
 	}
 
 }
